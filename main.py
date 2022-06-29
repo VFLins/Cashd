1cbc4b2383cba6c8803399b239af884b71c5a31e
Vitor Ferreira Lins <vflins@live.com>
2022-06-29 12:14:48 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import prompter as ppt
 import handler as hdl
-import db_manager as dm
+import db_manager as dbm
 
-print("Bem vindo ao Cashd, seus dados serão salvos em:", dm.WORK_DIR)
-print("\nAtalhos disponíveis:")
-for a, b in zip(*hdl.SHORTCUTS):
-    print(a.ljust(6), b)
+if __name__ == "__main__":
+    print("Bem vindo ao Cashd, seus dados serão salvos em:", dbm.WORK_DIR)
+    print("\nAtalhos disponíveis:")
+    for a, b in zip(*hdl.SHORTCUTS):
+        print(a.ljust(6), b)
 
-ppt.prompt_entry()
+    ppt.prompt_entry()
