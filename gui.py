1ee776a97b524e78f10a7b951cc8c21778c6d9a4
Vitor Ferreira Lins <vflins@live.com>
2022-07-09 12:32:45 -0300

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,11 +1,18 @@
 import tkinter as tk
+import tkcalendar
 from tkinter import ttk
+from ttkwidgets import autocomplete as tkac
 from tkinter.font import Font
 import db_manager as dbm
 
 FG_COLOR_ACTIVE = "#2e2e2e"
 FG_COLOR = "#55776f"
 BG_COLOR = "#f7f3f3"
+
+ACCOUNTS = [
+    "Yacouba Warin", "Iskra Sho", "Ansgar Asiri", 
+    "Eusebia Pawlu", "Rahul Andreas", "Rahul Andreas",
+    "Debora Aline"]
 
 # FrontPage Settings
 fpage = tk.Tk()
@@ -29,31 +36,45 @@ label_logo.place(x = 15, rely = 0)
 # Button Functions
 def button_click_ins():
     button_toggle_insert.configure(state = tk.DISABLED, bg = FG_COLOR)
+    button_toggle_accounts.configure(state = tk.NORMAL, bg = BG_COLOR)
     button_toggle_consult.configure(state = tk.NORMAL, bg = BG_COLOR)
     button_toggle_setting.configure(state = tk.NORMAL, bg = BG_COLOR)
 
+    frame_accounts.place_forget()
     frame_consult.place_forget()
     frame_configure.place_forget()
-
     frame_insert.place(y = 50, relwidth = 1)
-    frame_insert_draw()
-    
-    
-def button_click_con():
+
+def button_click_acc():
+    button_toggle_accounts.configure(state = tk.DISABLED, bg = FG_COLOR)
     button_toggle_insert.configure(state = tk.NORMAL, bg = BG_COLOR)
-    button_toggle_consult.configure(state = tk.DISABLED, bg = FG_COLOR)
+    button_toggle_consult.configure(state = tk.NORMAL, bg = BG_COLOR)
     button_toggle_setting.configure(state = tk.NORMAL, bg = BG_COLOR)
 
     frame_insert.place_forget()
-    frame_consult.place(y = 50, relwidth = 1)
+    frame_consult.place_forget()
     frame_configure.place_forget()
-
-def button_click_set():
+    frame_accounts.place(y = 50, relwidth = 1)
+    
+def button_click_con():
+    button_toggle_consult.configure(state = tk.DISABLED, bg = FG_COLOR)
+    button_toggle_accounts.configure(state = tk.NORMAL, bg = BG_COLOR)
     button_toggle_insert.configure(state = tk.NORMAL, bg = BG_COLOR)
-    button_toggle_consult.configure(state = tk.NORMAL, bg = BG_COLOR)
-    button_toggle_setting.configure(state = tk.DISABLED, bg = FG_COLOR)
+    button_toggle_setting.configure(state = tk.NORMAL, bg = BG_COLOR)
 
     frame_insert.place_forget()
+    frame_accounts.place_forget()
+    frame_configure.place_forget()
+    frame_consult.place(y = 50, relwidth = 1)
+
+def button_click_set():
+    button_toggle_setting.configure(state = tk.DISABLED, bg = FG_COLOR)
+    button_toggle_insert.configure(state = tk.NORMAL, bg = BG_COLOR)
+    button_toggle_accounts.configure(state = tk.NORMAL, bg = BG_COLOR)
+    button_toggle_consult.configure(state = tk.NORMAL, bg = BG_COLOR)
+        
+    frame_insert.place_forget()
+    frame_accounts.place_forget()
     frame_consult.place_forget()
     frame_configure.place(y = 50, relwidth = 1)
 
@@ -66,13 +87,25 @@ button_toggle_insert = tk.Button(
     bg = BG_COLOR, activebackground = FG_COLOR)
 button_toggle_insert.place(x = 150, y = 12)
 
+button_toggle_accounts = tk.Button(
+    fpage, command = button_click_acc,
+    text = "Accounts",
+    borderwidth = 0, font = Font(weight = "bold"),
+    fg = FG_COLOR, activeforeground = BG_COLOR, disabledforeground = BG_COLOR,
+    bg = BG_COLOR, activebackground = FG_COLOR)
+button_toggle_accounts.place(
+    in_ = button_toggle_insert, bordermode = "outside",
+    anchor = "nw", relx = 1, x = 10)
+
 button_toggle_consult = tk.Button(
     fpage, command = button_click_con,
     text = "Consult",
     borderwidth = 0, font = Font(weight = "bold"),
     fg = FG_COLOR, activeforeground = BG_COLOR, disabledforeground = BG_COLOR,
     bg = BG_COLOR, activebackground = FG_COLOR)
-button_toggle_consult.place(x = 215, y = 12)
+button_toggle_consult.place(
+    in_ = button_toggle_accounts, bordermode = "outside",
+    anchor = "nw", relx = 1, x = 10)
 
 button_toggle_setting = tk.Button(
     fpage, command = button_click_set,
@@ -80,7 +113,9 @@ button_toggle_setting = tk.Button(
     borderwidth = 0, font = Font(weight = "bold"),
     fg = FG_COLOR, activeforeground = BG_COLOR, disabledforeground = BG_COLOR,
     bg = BG_COLOR, activebackground = FG_COLOR)
-button_toggle_setting.place(x = 300, y = 12)
+button_toggle_setting.place(
+    in_ = button_toggle_consult, bordermode = "outside",
+    anchor = "nw", relx = 1, x = 10)
 
 label_greet = tk.Label(
     fpage,
@@ -93,7 +128,7 @@ separator_header = ttk.Separator(fpage, orient='vertical')
 separator_header.place(x = 135, y = 10, height = 30)
 
 separator_sections = ttk.Separator(fpage, orient='horizontal')
-separator_sections.place(y = 50, relwidth = 1)
+separator_sections.place(y = 49, relwidth = 1)
 
 ### Frames
 ###################
@@ -103,15 +138,30 @@ frame_insert = tk.Frame(fpage)
 
 label_ins_account = tk.Label(
     frame_insert, text = "Account name:",
-    bg = BG_COLOR,
-    width = 80, justify = "left")
-label_ins_account.pack(padx = (10, 0))
+    width = 30, bg = BG_COLOR, justify = "left")
+label_ins_account.pack(pady = (10, 0))
 
-entry_ins_account = tk.Entry(
-    )
+value_ins_account = tk.StringVar()
+value_ins_account.set("Choose an account")
+
+option_ins_account = tkac.AutocompleteCombobox(
+    frame_insert, completevalues = ACCOUNTS, 
+    background = BG_COLOR, width = 30)
+option_ins_account.pack()
+
+label_ins_date = tk.Label(
+    frame_insert, text = "Transaction's date:",
+    width = 30, bg = BG_COLOR, justify = "left")
+label_ins_date.pack(pady = (10, 0))
+
+date_ins = tkcalendar.DateEntry(frame_insert, width = 30)
+date_ins.pack()
+
+frame_accounts = tk.Frame(fpage)
 
 frame_consult = tk.Frame(fpage)
 
 frame_configure = tk.Frame(fpage)
 
+fpage.after_idle(button_click_ins)
 fpage.mainloop()
