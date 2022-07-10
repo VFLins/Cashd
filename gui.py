c4a3af119732a622bca7034531b3ff4a7185af40
Vitor Ferreira Lins <vflins@live.com>
2022-07-10 17:28:17 -0300

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,3 +1,4 @@
+from textwrap import fill
 import tkinter as tk
 import tkcalendar
 from tkinter import ttk
@@ -6,8 +7,8 @@ from tkinter.font import Font
 import db_manager as dbm
 
 FG_COLOR_ACTIVE = "#2e2e2e"
-FG_COLOR = "#55776f"
-BG_COLOR = "#f7f3f3"
+FG_COLOR = "#2e5077"
+BG_COLOR = "white"
 
 ACCOUNTS = [
     "Yacouba Warin", "Iskra Sho", "Ansgar Asiri", 
@@ -17,8 +18,8 @@ ACCOUNTS = [
 # FrontPage Settings
 fpage = tk.Tk()
 fpage.configure(bg = BG_COLOR)
-fpage.option_add("*font", "Segoe-UI 11")
-fpage.title("ainda ta feio kkkkkkk")
+fpage.option_add("*font", "Segoe-UI 14")
+fpage.title("não está mais tão feio")
 fpage.geometry("640x480")
 fpage.minsize(640, 480)
 
@@ -27,10 +28,10 @@ fpage.minsize(640, 480)
 label_logo = tk.Label(
     fpage,
     text = "Cashd",
-    font = Font(weight = "bold", size = 26),
+    font = Font(weight = "bold", size = 32),
     fg = "#2e2e2e",
     bg = BG_COLOR)
-label_logo.place(x = 15, rely = 0)
+label_logo.place(x = 15)
 
 ## Buttons
 # Button Functions
@@ -43,7 +44,7 @@ def button_click_ins():
     frame_accounts.place_forget()
     frame_consult.place_forget()
     frame_configure.place_forget()
-    frame_insert.place(y = 50, relwidth = 1)
+    frame_insert.place(y = 56, relwidth = 1, relheight = 1)
 
 def button_click_acc():
     button_toggle_accounts.configure(state = tk.DISABLED, bg = FG_COLOR)
@@ -54,7 +55,7 @@ def button_click_acc():
     frame_insert.place_forget()
     frame_consult.place_forget()
     frame_configure.place_forget()
-    frame_accounts.place(y = 50, relwidth = 1)
+    frame_accounts.place(y = 56, relwidth = 1, relheight = 1)
     
 def button_click_con():
     button_toggle_consult.configure(state = tk.DISABLED, bg = FG_COLOR)
@@ -65,7 +66,7 @@ def button_click_con():
     frame_insert.place_forget()
     frame_accounts.place_forget()
     frame_configure.place_forget()
-    frame_consult.place(y = 50, relwidth = 1)
+    frame_consult.place(y = 56, relwidth = 1, relheight = 1)
 
 def button_click_set():
     button_toggle_setting.configure(state = tk.DISABLED, bg = FG_COLOR)
@@ -76,21 +77,23 @@ def button_click_set():
     frame_insert.place_forget()
     frame_accounts.place_forget()
     frame_consult.place_forget()
-    frame_configure.place(y = 50, relwidth = 1)
+    frame_configure.place(y = 56, relwidth = 1, relheight = 1)
 
 # Button Widgets
 button_toggle_insert = tk.Button(
     fpage, command = button_click_ins,
     text = "Insert",
-    borderwidth = 0, font = Font(weight = "bold"),
+    borderwidth = 0, font = Font(weight = "bold", size = 16),
     fg = FG_COLOR, activeforeground = BG_COLOR, disabledforeground = BG_COLOR,
     bg = BG_COLOR, activebackground = FG_COLOR)
-button_toggle_insert.place(x = 150, y = 12)
+button_toggle_insert.place(
+    in_ = label_logo, bordermode = "outside",
+    anchor = "nw", relx = 1, x = 20, y = 8)
 
 button_toggle_accounts = tk.Button(
     fpage, command = button_click_acc,
     text = "Accounts",
-    borderwidth = 0, font = Font(weight = "bold"),
+    borderwidth = 0, font = Font(weight = "bold", size = 16),
     fg = FG_COLOR, activeforeground = BG_COLOR, disabledforeground = BG_COLOR,
     bg = BG_COLOR, activebackground = FG_COLOR)
 button_toggle_accounts.place(
@@ -100,7 +103,7 @@ button_toggle_accounts.place(
 button_toggle_consult = tk.Button(
     fpage, command = button_click_con,
     text = "Consult",
-    borderwidth = 0, font = Font(weight = "bold"),
+    borderwidth = 0, font = Font(weight = "bold", size = 16),
     fg = FG_COLOR, activeforeground = BG_COLOR, disabledforeground = BG_COLOR,
     bg = BG_COLOR, activebackground = FG_COLOR)
 button_toggle_consult.place(
@@ -110,7 +113,7 @@ button_toggle_consult.place(
 button_toggle_setting = tk.Button(
     fpage, command = button_click_set,
     text = "Settings",
-    borderwidth = 0, font = Font(weight = "bold"),
+    borderwidth = 0, font = Font(weight = "bold", size = 16),
     fg = FG_COLOR, activeforeground = BG_COLOR, disabledforeground = BG_COLOR,
     bg = BG_COLOR, activebackground = FG_COLOR)
 button_toggle_setting.place(
@@ -125,10 +128,12 @@ label_greet
 
 # Separators
 separator_header = ttk.Separator(fpage, orient='vertical')
-separator_header.place(x = 135, y = 10, height = 30)
+separator_header.place(
+    in_ = label_logo, bordermode = "outside", height = 30,
+    anchor = "nw", relx = 1, x = 6, y = 12)
 
 separator_sections = ttk.Separator(fpage, orient='horizontal')
-separator_sections.place(y = 49, relwidth = 1)
+separator_sections.place(y = 54, relwidth = 1)
 
 ### Frames
 ###################
@@ -136,26 +141,40 @@ separator_sections.place(y = 49, relwidth = 1)
 ## "Insert" frame
 frame_insert = tk.Frame(fpage)
 
-label_ins_account = tk.Label(
+ins_label_account = tk.Label(
     frame_insert, text = "Account name:",
-    width = 30, bg = BG_COLOR, justify = "left")
-label_ins_account.pack(pady = (10, 0))
+    width = 30, justify = "left")
+ins_label_account.pack(pady = (10, 0))
 
-value_ins_account = tk.StringVar()
-value_ins_account.set("Choose an account")
+ins_option_account = tkac.AutocompleteCombobox(
+    frame_insert, completevalues = ACCOUNTS, width = 30)
+ins_option_account.pack()
 
-option_ins_account = tkac.AutocompleteCombobox(
-    frame_insert, completevalues = ACCOUNTS, 
-    background = BG_COLOR, width = 30)
-option_ins_account.pack()
-
-label_ins_date = tk.Label(
+ins_label_date = tk.Label(
     frame_insert, text = "Transaction's date:",
-    width = 30, bg = BG_COLOR, justify = "left")
-label_ins_date.pack(pady = (10, 0))
+    width = 30, justify = "left")
+ins_label_date.pack(pady = (10, 0))
 
-date_ins = tkcalendar.DateEntry(frame_insert, width = 30)
-date_ins.pack()
+ins_option_date = tkcalendar.DateEntry(
+    frame_insert, width = 30, relief = "solid",
+    background = FG_COLOR, selectbackground = FG_COLOR)
+ins_option_date.pack()
+
+ins_label_value = tk.Label(
+    frame_insert, text = "Value (positive for income):",
+    width = 30, justify = "left")
+ins_label_value.pack(pady = (10, 0))
+
+ins_option_value = tk.Entry(
+    frame_insert, text = "0000,00",
+    width = 32, relief = "solid")
+ins_option_value.insert(0, "0,00")
+ins_option_value.pack()
+
+ins_ok_button = tk.Button(frame_insert, text = "Confirm")
+ins_ok_button.place(
+    in_ = ins_option_value, bordermode = "outside",
+    anchor = "se", relx = 1, rely = 1, y = 40)
 
 frame_accounts = tk.Frame(fpage)
 
